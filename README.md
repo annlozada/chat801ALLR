1e065ae9f52f85116a3924327facc73c3df3ff0c
Luis Angel <luismx1123@gmail.com>
2021-07-20 23:12:33 -0500

    new chat

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# rickmrdwp.github.io
-Ejemplo de un chat y acceso a Firebase (Firestore con Google)
+# Chat con firebase
+| Tarea | Link |
+| ------ | ------ |
+| Autenticacion | [Ver tarea](https://annlozada.github.io/firebase/) |
+| GitHub | [plugins/github/README.md][PlGh] |
+## License
+
+Ana Laura Lozada Rojas
--- js/init.js
+++ js/init.js
@@ -1,12 +1,12 @@
 /*  Initializa Firebase con la configuración del proyecto. Revisa la
- * configuración en tu servidor de Firebase.  */
+ * configuración en tu servidor de Firebase.*/
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDRFoOEt36BQTfAdX69Z9RB1LbXENgNpXI",
-  authDomain: "chatdwp.firebaseapp.com",
-  projectId: "chatdwp",
-  storageBucket: "chatdwp.appspot.com",
-  messagingSenderId: "596054803702",
-  appId: "1:596054803702:web:2eb541bf84510a206bd8d7",
-  measurementId: "G-YTKR711Y8Z"
-    });
+  apiKey: "AIzaSyChJLNREsr1Ad4BaLvGuqJY-iaLX5ls3q0",
+  authDomain: "loggin-13bbc.firebaseapp.com",
+  projectId: "loggin-13bbc",
+  storageBucket: "loggin-13bbc.appspot.com",
+  messagingSenderId: "1091837225170",
+  appId: "1:1091837225170:web:2ac8f13cbce24fcddfe50b",
+  measurementId: "G-CESKWDKBG7"
+});
